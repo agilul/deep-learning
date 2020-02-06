ad3bcfe49371993e605d85fed73b3321906823a0
Wong Yat Hin <yathin.wong1@gmail.com>
2020-02-06 12:57:01 +0100

    predict

--- app/guitars/guitars.py
+++ app/guitars/guitars.py
@@ -1,2 +1,11 @@
+from fastai.vision import defaults, torch, Path, open_image, load_learner
+
+defaults.device = torch.device("cpu")
+path = Path("app/guitars")
+img = open_image(path/"myguitar.jpg")
+learn = load_learner(path)
+
+
 async def handle():
-    return "ok"
+    pred_class, pred_idx, outputs = learn.predict(img)
+    return pred_class
